b6ec7c92d26308eb19bb839842839147cf0ea828
Joseph Jarman <57240500+Jarmander333@users.noreply.github.com>
2021-09-15 16:20:40 -0500

    Update num_analyzer.py
    Forgot a pound sign, and it broke the program (kind of self-explanatory how)

--- num_analyzer.py
+++ num_analyzer.py
@@ -14,5 +14,5 @@ else: # If it's not negative, it's positive
 
 if -100 > test_num > 100: # Checks if the number is within -100 to 100
     print("Test 3: Exclusive")
-else: If it's not, it's outside the range
+else: # If it's not, it's outside the range
     print("Test 3: Inclusive")
